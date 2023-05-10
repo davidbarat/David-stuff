13b5a92669443843d1a6aa6a3acb076775339560
David <33658472+davidbarat@users.noreply.github.com>
2023-05-10 14:56:05 +0200

    Update schema.sql

--- License2sell/schema.sql
+++ License2sell/schema.sql
@@ -1,1 +1,21 @@
+DROP TABLE IF EXISTS Institute;
+DROP TABLE IF EXISTS CodeSource;
 
+CREATE TABLE Institute (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  InstituteName TEXT NOT NULL,
+  Certification TEXT NOT NULL,
+  DateCertification TIMESTAMP NOT NULL,
+  idSource INTEGER,
+  CONSTRAINT fk_codeSource,
+  FOREIGN KEY (idSource),
+  REFERENCES codeSource(idSource),
+  UNIQUE(InstituteName, CertificationCode, DateCertification, idSource)
+  );
+  
+  CREATE TABLE CodeSource (
+    idSource INTEGER PRIMARY KEY AUTOINCREMENT,
+    SourceName TEXT NOT NULL
+    );
+    
+ CREATE UNIQUE INDEX UNQ_InstituteName ON Institute(InstituteName);
